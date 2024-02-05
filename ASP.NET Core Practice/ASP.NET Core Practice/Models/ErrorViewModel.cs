0f06aa1dce9c37cd76e6af3c0d6003f7e4b91949
Areeba Amjad <areeba.amjad2020@gmail.com>
2024-02-05 07:22:53 -0800

    Add project files.

---
+++ ASP.NET Core Practice/ASP.NET Core Practice/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASP.NET_Core_Practice.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
