31f515beea3275d19703439290367df986b9703f
Areeba Amjad <areeba.amjad2020@gmail.com>
2024-03-30 23:58:05 -0700

    Commit 18 : Tag Helpers = asp-controller | asp-action | ASP.Net Tag Helpers

---
+++ TagHelpersDemo/TagHelpersDemo/Notes/Notes.cs
@@ -0,0 +1,17 @@
+﻿
+// ========================== 26 ============================
+/*
+ *  Creating HyperLinks using Tag Helpers
+ * 
+ *  In MVC 5: HTML Helpers : They are methods 
+ *  In .NET: Tag Helpers : To render HTML on server side: They are attributes
+ *  When we use HTML Helpers it renders the HTML on the server side cuz .NET Core works on the server side
+ *  
+ *  Diff btw @HTML.ActionLink and @Url.Action ? 
+ *  @HTML.ActionLink generate the whole anchor | @Url.Action generates the href not the whole a
+ *  
+ *  When we click on any Link an Id is generated and that Id will take us to View 
+ *  We usually do the Id work with Edit or Delete
+ *  
+ * 
+ */
